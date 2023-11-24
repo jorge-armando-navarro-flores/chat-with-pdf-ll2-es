b0a805c47942a85b780085cc4785506a97f8c7f3
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2023-11-24 10:46:55 -0600

    gpu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import multiprocessing
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OllamaEmbeddings
@@ -7,6 +8,10 @@ from langchain.chains import RetrievalQA
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.llms import Ollama
+
+
+def process_chunk(chunk, embeddings):
+    return embeddings.embed(chunk)
 
 
 if __name__ == "__main__":
@@ -20,10 +25,24 @@ if __name__ == "__main__":
     print(len(texts))
 
     embeddings = OllamaEmbeddings(base_url="http://localhost:11434", model="orca-mini")
-    vectorstore = FAISS.from_documents(texts, embeddings)
+    vectorstore = FAISS()
+
+    # Specify the number of GPUs to use
+    num_gpus = 2
+
+    # Create a pool of processes for parallel processing
+    pool = multiprocessing.Pool(processes=num_gpus)
+
+    # Process the chunks in parallel using multiple GPUs
+    embeddings_list = pool.starmap(process_chunk, [(chunk, embeddings) for chunk in texts])
+
+    # Add the embeddings to the vector store
+    for i, embedding in enumerate(embeddings_list):
+        vectorstore.add_document(embedding, texts[i])
+
     vectorstore.save_local("faiss_index_react")
 
-    new_vectorstore = FAISS.load_local("faiss_index_react", embeddings)
+    new_vectorstore = FAISS.load_local("faiss_index_react")
 
     qa = RetrievalQA.from_chain_type(
         llm=Ollama(
@@ -38,4 +57,4 @@ if __name__ == "__main__":
     result = qa({"query": query})
     print(result)
 
-    # llama2 vdim=5120
+    # llama2 vdim=5120" beginner friendly
